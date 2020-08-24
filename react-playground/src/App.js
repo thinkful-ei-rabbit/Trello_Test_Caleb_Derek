fe802c75b31d59eb55aef6a30336a3d7985f4600
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-08-24 12:28:59 -0400

    first commit

---
+++ react-playground/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Split from './composition/Split';
+import './App.css';
+import Tooltip from './composition/Tooltip';
+
+// make 2 tooltips here and another inside the App directly
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
+
+function App() {
+  return (
+    <main className='App'>
+      <Split className='left' flexBasis='2'>
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        {/* make another tooltip directly inside the App */}
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  )
+}
+
+export default App
